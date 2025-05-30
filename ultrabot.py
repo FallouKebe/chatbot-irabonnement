325d65d5f826777f98b33f205f27b4cdb0418092
Serigne Fallou Kebe <falloukebe0710@gmail.com>
2025-05-30 09:56:59 +0000

    Implémentation complète chatbot v5 - integration anti spam

--- ultrabot.py
+++ ultrabot.py
@@ -9,8 +9,8 @@ class ultraChatBot():
     def __init__(self, json):
         self.json = json
         self.dict_messages = json['data']
-        self.ultraAPIUrl = 'https://api.ultramsg.com/instance122729/'
-        self.token = 'rjasbdgk5ff8aoal'
+        self.ultraAPIUrl = 'https://api.ultramsg.com/instance114147/'
+        self.token = 'e9gigjx981vwnnzh'
         
         # SOLUTION PERSISTANCE : Fichier pour sauvegarder les sessions
         self.sessions_file = 'user_sessions.json'
